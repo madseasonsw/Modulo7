cfed17035ada6bf3708d3c91b66805579eaf9321
madseasonsw <denismedina.e@gmail.com>
2023-05-17 19:00:19 -0400

    individual2

--- Siete/admin.py
+++ Siete/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Etiqueta  # Asegúrate de que Etiqueta está importado desde models
 
-# Register your models here.
+admin.site.register(Etiqueta)  # Esto registra el modelo Etiqueta en el administrador
--- Siete/models.py
+++ Siete/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Etiqueta(models.Model):
+    nombre = models.CharField(max_length=50)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+class Tarea(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_finalizacion = models.DateTimeField(null=True, blank=True)
+    completada = models.BooleanField(default=False)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    etiquetas = models.ManyToManyField(Etiqueta, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
